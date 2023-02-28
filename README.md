9f4e9445ac20898f094ca48912a83bec2494aa70
FranHutinel <m.ohutinel@gmail.com>
2023-02-27 21:35:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+javascript-fullstack- Reto módulo 6 perteneciente al curso Javascript Fullstack.
+
+La entrega consiste en desarrollar un servicio web en node el cual responda a peticiones HTTP de tipo GET y POST. El servicio administrará cartones de un juego de números aleatorios como de lotería. Los métodos HTTP deberán retornar distintas funciones.
+
+
+Agrupación del proyecto
+
+🚀La estructura de carpetas del proyecto es la siguiente:
+
+Documento index.html es la raíz del proyecto.
+
+Construido con 🛠
+
+html, css, js, Nodejs.
+
+Autor. Mariela Ochoa
---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let cartones = [];
+
+// Generar los primeros 5 cartones
+for (let i = 1; i <= 5; i++) {
+  let carton = {
+    id: i,
+    numeros: generarNumeros(),
+  };
+  cartones.push(carton);
+}
+
+// Función para generar números aleatorios sin repetirse
+function generarNumeros() {
+  let numeros = [];
+  while (numeros.length < 15) {
+    let numero = Math.floor(Math.random() * 30) + 1;
+    if (!numeros.includes(numero)) {
+      numeros.push(numero);
+    }
+  }
+  return numeros;
+}
+
+// GET: devuelve la lista completa de cartones generados
+app.get('/cartones', (req, res) => {
+  res.json(cartones);
+});
+
+// POST: crea un nuevo cartón
+app.post('/cartones', (req, res) => {
+  let carton = {
+    id: cartones.length + 1,
+    numeros: generarNumeros(),
+  };
+  cartones.push(carton);
+  res.json(carton);
+});
+
+// Levantar el servidor
+app.listen(3000, () => {
+  console.log('Servicio de cartones escuchando en el puerto 3000');
+});
